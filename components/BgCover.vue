33d97726217747ff48fbeeefbb369530208baab1
Colm McSharry <colmmcsharry@Admins-MacBook-Air.local>
2022-05-27 14:27:25 +0100

    Revert "cleaning"
    This reverts commit 647d6ce693a04e7a8fa25fee15981325bcc0b988.

---
+++ components/BgCover.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  .cover
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+/* .cover {
+  width:100vw;
+  height:100vh;
+  z-index:-2;
+  position:absolute;
+  top:0px;
+  left:0px;
+  background-image:url('../assets/terenureinn.jpeg')
+}
+
+.cover:after{
+  content:"";
+   width:100vw;
+  height:100vh;
+  background-color:rgba(20, 14, 15, 0.63);
+  position:absolute;
+  top:0px;
+  left:0px;
+  
+} */
+
+.cover {
+  position:absolute;
+  width:100%;
+  height:100%;
+  background-color:red;
+  z-index: -2;
+}
+
+</style>
