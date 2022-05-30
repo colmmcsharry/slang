68e356fa6d769fde6078506517b4c25438dfe045
Colm McSharry <colmmcsharry@Admins-MacBook-Air.local>
2022-05-30 17:37:58 +0100

    excess files deleted

--- store/index.js
+++ store/index.js
@@ -1,7 +1,4 @@
 export const state = () => ({
-  tasks: [],
-  counter: 0,
-  cartItems: [],
   category: 'Greetings',
   country: 'Irish',
   faveQuotes: [],
@@ -249,27 +246,5 @@ FAVE(state, quote){
   console.log('helloo', quote);
   quote.isFavourite = !quote.isFavourite;
   state.faveQuotes = [ ...state.faveQuotes, quote ]
-  },
-
-  ADDTOCART(state, item) {
-    state.cartItems = [item, ...state.cartItems]
-  },
-  REMOVEITEM(state, item) {
-    state.cartItems.splice(state.cartItems.indexOf(item), 1)
-  },
-
-  // todolist starts
-  ADD_TASK(state, task) {
-    state.tasks = [{ content: task, done: false }, ...state.tasks]
-  },
-  REMOVE_TASK(state, task) {
-    state.tasks.splice(state.tasks.indexOf(task), 1)
-  },
-  TOGGLE_TASK(state, task) {
-    task.done = !task.done
-  },
-  // todo list ends
-  INCREMENT(state) {
-    state.counter++
-  },
+  }
 }
